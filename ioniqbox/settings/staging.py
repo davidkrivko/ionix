1492a57d46b105be2d631a3a97031460a74e3bae
Davyd Kryvko <davidkrivko@gmail.com>
2023-02-20 14:36:58 +0100

    delete hosts

--- ioniqbox/settings/staging.py
+++ ioniqbox/settings/staging.py
@@ -13,9 +13,7 @@ ALLOWED_HOSTS = [
     'api-staging.turnonheat.com',
     'ioadmin-staging.turnonheat.com',
     'panel-staging.turnonheat.com',
-    'api-do.147.182.140.74',
-    'admin-do.147.182.140.74',
-    'panel-do.147.182.140.74',
+    '147.182.140.74',
 ]
 
 CORS_ALLOWED_ORIGINS = ['https://app.turnonheat.com']
